0a1e923ba61f04c9549e8cb1fa796e21c833e1db
thekeongspeed <danifadlan313@gmail.com>
2025-07-30 19:54:35 +0700

    fix file viewer

--- src/components/InstrumenPembelajaran.vue
+++ src/components/InstrumenPembelajaran.vue
@@ -436,7 +436,7 @@
 
                           <ul v-else>
                             <li v-for="(file, index) in uploadedFiles" :key="index" class="file-item">
-                              <a :href="file.path" target="_blank" class="file-link">
+                              <a :href="getFullFileUrl(file.path)" target="_blank" rel="noopener noreferrer" class="file-link">
                                 <span class="file-icon">
                                   <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M14 2H6C5.46957 2 4.96086 2.21071 4.58579 2.58579C4.21071 2.96086 4 3.46957 4 4V20C4 20.5304 4.21071 21.0391 4.58579 21.4142C4.96086 21.7893 5.46957 22 6 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V8L14 2Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -736,6 +736,14 @@ const fetchUploadedFiles = async () => {
     }
   }
 };
+
+
+function getFullFileUrl(filePath) {
+
+  const baseURL = import.meta.env.VITE_API_BASE_URL; 
+  return `${baseURL}/uploads/instrumen/${filePath}`; 
+}
+
 
 
 async function fetchInstrumentData(kelas, jenis) {
